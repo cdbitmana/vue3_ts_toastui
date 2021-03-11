b2cfb775b99b31932161ea140f126b4fd75f51a0
jhs512 <jangka512@gmail.com>
2021-03-11 10:45:46 +0900

    토스트 UI 에디터 실행

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <ToastUIEditor :height="200" />
+
+  <hr>
+  <hr>
+  <hr>
+
+  <ToastUIEditor :height="200" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import ToastUIEditor from './components/TUIEditor.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    ToastUIEditor
+  }
+})
+</script>
+
+<style>
+</style>
---
+++ src/components/TUIEditor.vue
@@ -0,0 +1,49 @@
+<template>
+  <div ref="groundElRef"></div>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent, onMounted } from 'vue'
+// toastUi 불러오기
+import Editor from '@toast-ui/editor';
+import 'codemirror/lib/codemirror.css'; // Editor's Dependency Style
+import '@toast-ui/editor/dist/toastui-editor.css'; // Editor's Style
+import Viewer from '@toast-ui/editor/dist/toastui-editor-viewer';
+import '@toast-ui/editor/dist/toastui-editor-viewer.css';
+
+// 제이쿼리 불러오기
+import $ from 'jquery';
+
+export default defineComponent({
+  name: 'ToastUIEditor',
+  props: {
+    height: {
+      type: Number,
+      default:500
+    }
+  },
+  setup: (props) => {
+    const groundElRef = ref<HTMLInputElement>();
+
+    onMounted(() => {
+      if ( !!!groundElRef.value ) {
+        return;
+      }
+
+      const editor = new Editor({
+        el: groundElRef.value,
+        previewStyle: undefined,
+        initialValue: "# 안녕",
+        height: props.height + "px"
+      });
+    });
+
+    return {
+      groundElRef
+    }
+  }
+})
+</script>
+
+<style scoped>
+</style>
